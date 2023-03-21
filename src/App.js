70ba56d89b7f48e6e7c9d34f4fe4105afc28c95d
mosabbir.duet <mosabbir.duet@gmail.com>
2023-03-21 15:29:36 +0600

    Pass dynamic data to components

--- src/App.js
+++ src/App.js
@@ -6,28 +6,29 @@ function App() {
     <div className="App">
       {/* Add Component  */}
       <h1>Add Simple Component <code>Similar in look and data</code></h1>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name = "Mosabbir Hossain" email = "abc@gmail.com"></Person>
+      <Person name = "Masud Rana" email = "xyz@gmail.com"></Person>
+      <Person name = "Tareq Rahman" email = "pqr@gmail.com"></Person>
+      <Person name = "Shabuz" email = "tyu@gmail.com"></Person>
       <h2 style={{fontSize : "2.5rem", borderBottom: '3px solid co', paddingBottom: '5px', fontWeight: 'bold'}}>Component of Friends</h2>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
+      {/* <Friend name = "Mosabbir Hossain" number = '0' ></Friend> */}
+      <Friend name = "Masud Rana" number = '4' gfName = {["halima","galima","jalima","nilima"]}></Friend>
+      <Friend name = "Tareq" number = '3' gfName = {["sadia","kadia","madia"]}></Friend>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   return (
     <div className='person-info'>
-      <h1>Mosabbir Hossain</h1>
-      <p>Assistant Programmer</p>
+      <h1>{props.name}</h1>
+      <p>{props.email}</p>
     </div>
   )
 }
 
-function Friend() {
+function Friend(props) {
+  console.log(props)
   return (
     <div style={{backgroundColor: 'coral',
       color: 'white',
@@ -36,8 +37,11 @@ function Friend() {
       margin: '10px',
       borderRadius: '10px'
       }}>
-      <h1>Tareq Rahman</h1>
-      <p>Instructor</p>
+      <h1>{props.name} has {props.number !== '0'? props.number: "No"} girlfriends.</h1>
+      <p>Name: {props.gfName.length !== 0? props.gfName.map(name => {
+          return (name);
+          
+        }).join(", ") : "Single"}</p> <br />
     </div>
   )
 }
